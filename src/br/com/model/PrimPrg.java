71d574766509b68bb2e3b56ce095622a15a5e894
cmadurei <cmadurei@gmail.com>
2021-12-22 20:59:22 -0300

    Atualizando conquista do Pentacampeonato SUB-20

--- src/br/com/model/PrimPrg.java
+++ src/br/com/model/PrimPrg.java
@@ -5,6 +5,7 @@ public class PrimPrg {
 	public static void main(String[] args) {
 		Integer vezes = 3;
 		System.out.println("Palmeiras ganhou " + (vezes) + " vezes a Libertadores!");
+		System.out.println("Pentacampeão SUB-20");
 	}
 
 }
