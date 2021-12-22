b7228e63a4b4989800e680dbe19ca0e814fd75a3
cmadurei <cmadurei@gmail.com>
2021-12-22 18:32:30 -0300

    Primeiro commit

---
+++ src/br/com/model/PrimPrg.java
@@ -0,0 +1,10 @@
+package br.com.model;
+
+public class PrimPrg {
+
+	public static void main(String[] args) {
+		Integer vezes = 3;
+		System.out.println("Palmeiras ganhou " + (vezes) + " vezes a Libertadores!");
+	}
+
+}
